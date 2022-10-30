c6dff063265a76b88bfbadd7c14a7d4b97aec4cb
Jacqueline Gellick <jgellick@gmail.com>
2022-10-30 21:22:57 +0100

    Completed Day 01 Exercises

---
+++ README.md
@@ -0,0 +1,10 @@
+## 30 Days of Javascript: Day 01
+
+## Purpose
+The purpose of this segment of 30 Days of Javascript was to:
+- get introduced to Javascript
+- install required programs
+- practice declaring variables, checking data types, and printing to the console
+
+### My thoughts
+- I already had the needed programs installed on my computer, so I was able to skip the first steps. The exercises were easy, as I had completed similar exercises before. 
---
+++ day-01.js
@@ -0,0 +1,69 @@
+// Declaring different varialbes of different data types
+// let firstName = "Jackie";
+// let lastName = "Gellick";
+// let country = "Spain";
+// let city = "Bilbao";
+// let age = 33;
+// let isMarried = true;
+
+// console.log(firstName, lastName, country, city, age, isMarried);
+
+// // Declaring variables with number values
+// const gravity = 9.81 // earth gravity in m/s2
+// const boilingPoint = 100 // water boiling point, temperature in ºC
+// const PI = 3.14 // geometrical constant
+
+// console.log(gravity, boilingPoint, PI);
+
+// DAY 01 EXERCISES
+// 1. Write a single line comment which says, comments can make code readable
+// comments can make codes readable
+
+// 2. Write another single comment whcih says, Welcome to 30DaysOfJavascript
+// Welcome to 30DaysOfJavascript
+
+// 3. Write a multiline comment which says, comments can make code readable, easy to reuse, and informative
+/* Comments can make code:
+- readable, 
+- easy to reuse, 
+- informative 
+*/
+
+// 4. Declare variables and assign string, boolean, undefined, and null data types
+// const name = "Jackie";
+// const isMarried = true;
+// const isCoder = null;
+// const poodles = undefined;
+
+// 5. Check different data types
+// console.log(typeof name, typeof isMarried, typeof isCoder, typeof poodles);
+
+// 6. Declare four variables without assigning values
+// const firstName; 
+// const lastName;
+// let age;
+// const poodleName;
+// const profession;
+
+// 7. Declare four variables with assigned values
+// const firstName = "Jackie";
+// const lastName = "Gellick";
+// let age = 33;
+// const poodleName  = "Winston";
+// const profession = "Speech-Language Pathologist";
+
+// 8. Declare variables to store your first name, marital status, country, and age in multiple lines
+// const firstName = "Jackie";
+// const lastName = "Gellick";
+// let isMarried = true;
+// let country = "Spain";
+// let age = 33;
+
+// 9. Declare variables to store your first name, last name, marital status, country, and age in a single line
+// const firstName = "Jackie", lastName = "Gellick", isMarried = true, country = "Spain", age = 33;
+
+// 10. Declare two variables myAge and yourAge and assign them initial values and log to browser console.
+// let myAge = 33;
+// let yourAge = 37;
+
+// console.log(`My age is ${myAge} years old. You are ${yourAge} years old.`);
