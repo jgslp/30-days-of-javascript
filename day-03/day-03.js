f497991ed89b192faad049251b419b169d4d72fc
Jacqueline Gellick <jgellick@gmail.com>
2022-10-31 13:59:35 +0100

    Completed exercise 1-4

--- day-03/day-03.js
+++ day-03/day-03.js
@@ -18,3 +18,18 @@
 
 // console.log('10' === 10); // yields false;
 // console.log('10' == 10); // yields true;
+
+// 3. Check if parseInt('9.8') is equal to 10;
+
+// console.log(parseInt('9.8') === 10); // yields false
+
+// 4. Boolean value is eitehr true or false. 
+// Write three Javascript statements which provide a truthy value.
+// Write three Javascript statements which provide a falsy value.
+
+// console.log(3 === 6/2); // yields true
+// console.log(10 >= 8); // yields true
+// console.log(3 == "3"); // yields true
+// console.log(10 === null); // yields false
+// console.log(5 > 10); // yields false
+// console.log(3 === "three"); // yields false
