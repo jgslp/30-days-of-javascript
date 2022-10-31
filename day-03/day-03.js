6632e7ba6defddcf94e7ac2e361b261bdbb6a744
Jacqueline Gellick <jgellick@gmail.com>
2022-10-31 14:06:04 +0100

    Completed exercise 1-5

--- day-03/day-03.js
+++ day-03/day-03.js
@@ -33,3 +33,31 @@
 // console.log(10 === null); // yields false
 // console.log(5 > 10); // yields false
 // console.log(3 === "three"); // yields false
+
+// 5. Figure out the result of the following comparison expression first wihtout using console.log, then confirm it with console.log
+// i. (4 > 3) should yield true
+// console.log(4 > 3); // true; correct
+// // ii. (4 >= 3) should yield true
+// console.log(4 >= 3); // true, correct
+// // iii. (4 < 3) should yield false
+// console.log(4 < 3); // false, correct
+// // iv. (4 <= 3) should yield false
+// console.log(4 <= 3); // false, correct
+// // v. (4 == 4) should yield true
+// console.log(4 == 4); // true, correct
+// // vi. (4 === 4) should yield true
+// console.log(4 === 4); // true, correct
+// // vii. (4 != 4) should yield false
+// console.log(4 != 4); // false, correct
+// // viii. (4 !== 4) should yield false
+// console.log(4 !== 4); // false, correct
+// // ix. (4 != '4') should yield false
+// console.log(4 != '4'); // false, correct
+// // x. (4 == '4) should yield true
+// console.log(4 == '4'); // true, correct
+// // xi. (4 === '4') should yield false
+// console.log(4 === '4'); // false, correct
+// // xii. find the length of python and jargon and make a falsy comparison statement
+// let python = "python";
+// let jargon = "jargon";
+// console.log(python.length > jargon.length); // yields false
