992e30e73ea2a2d642601871a004f363b160b005
Jacqueline Gellick <jgellick@gmail.com>
2022-10-31 13:46:47 +0100

    Completed Exercise 1-1

---
+++ day-03/day-03.js
@@ -0,0 +1,15 @@
+// 30 DAYS OF JAVASCRIPT DAY 03
+
+// Exercises: Level 1
+
+// 1. Declare firstName, lastName, country, city, age, isMarried, year variable and assign value to it adn use the typeof operator to check different data types.
+
+// let firstName = "Jackie";
+// let lastName = "Gellick";
+// let country = "Spain";
+// let city = "Bilbao";
+// let age = 33;
+// let isMarried = true;
+// let year = 2022;
+
+// console.log(typeof firstName, typeof lastName, typeof country, typeof city, typeof age, typeof isMarried, typeof year);
