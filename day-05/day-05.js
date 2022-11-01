6346d79a44e816b3c5268d6e0bb86ea399845fb9
Jacqueline Gellick <jgellick@gmail.com>
2022-11-01 12:37:40 +0100

    Completed Exercises level 1 numbers 1-4

---
+++ day-05/day-05.js
@@ -0,0 +1,51 @@
+// 30 Days of JavaScript: Day 05
+
+// EXERCISES - LEVEL 1
+// 1. Declare an empty array;
+// let array = [];
+
+// 2. Declare an array with more than 5 number of elements
+let countriesVisited = [
+    "USA", 
+    "Canada", 
+    "Bahamas", 
+    "Argentina",
+    "UK", 
+    "France", 
+    "Italy", 
+    "Vatican", 
+    "Spain", 
+    "Morocco", 
+    "Portugal", 
+    "Germany", 
+    "Slovakia", 
+    "Austria", 
+    "Hungary", 
+    "Czechia", 
+    "Belgium", 
+    "Switzerland", 
+    "Netherlands", 
+    "Croatia", 
+    "Bosnia & Herzegovina"];
+
+// 3. Find the length of your array.
+// console.log(countriesVisited.length); // returns 21
+
+// // 4. Get the first item, middle item, and last item of the array
+
+// // First item 
+// console.log(countriesVisited[0]); // returns USA
+
+// // Last item
+// console.log(countriesVisited[countriesVisited.length - 1]); // returns Bosnia & Herzegovina
+
+// // Middle item
+// let middle;
+
+// if (countriesVisited.length % 2 === 1) {
+//     middle = (countriesVisited.length - 1) / 2;
+// } else {
+//     middle = (countriesVisited.length / 2);
+// }
+ 
+// console.log(countriesVisited[middle]); // yields Portugal
