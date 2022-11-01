20ac8b6875ca3bcae52493ce9d0d8a410e5e8b14
Jacqueline Gellick <jgellick@gmail.com>
2022-11-01 12:16:15 +0100

    Completed exercise 2-2

--- day-04/day-04.js
+++ day-04/day-04.js
@@ -73,3 +73,16 @@
 // } else {
 //     console.log("Your grade is an F");
 // }
+
+// 2. Check if the season is autumn, winter, sprint, or summer
+// let month = "March"
+
+// if (month === "September" || month === "October" || month === "November") {
+//     console.log("The season is Autumn");
+// } else if (month === "December" || month === "January" || month === "February") {
+//     console.log("The season is Winter");
+// } else if (month === "March" || month === "April" || month === "May") {
+//     console.log("The season is Spring");
+// } else {
+//     console.log("The season is Summer");
+// }
