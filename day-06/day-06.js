3acd8e369931803c1321d24b57b627e7b9c8da9b
Jacqueline Gellick <jgellick@gmail.com>
2022-11-01 23:03:57 +0100

    Completed exercise level 1-1

---
+++ day-06/day-06.js
@@ -0,0 +1,35 @@
+// 30 DAYS OF JAVASCRIPT - DAY 06
+
+// EXERCISES LEVEL 1
+
+// 1. Iterate 0 to 10 using a for loop, while loop, and do while loop.
+// FOR LOOP
+
+// let count = 0;
+// for (i = 0; i <= 10; i++) {
+//     console.log(count ++)
+// }
+
+// WHILE LOOP
+// let count = 0;
+// while (count <= 10) { 
+//     console.log(count++);
+// }
+
+// DO WHILE LOOP
+// let count = 0;
+// do {
+//     console.log(count++)
+// } while (count <= 10);
+
+// 2. Iterate 10 to 0 using a for loop, while loop, and do while loop.
+
+// 3. Iterate 0 to n using a for loop
+
+// 4. Write a loop that makes the following pattern using console.log();
+
+// 5. Use a loop to print the following pattern:
+
+// 6. Using loop print the following pattern
+
+// 7. 
