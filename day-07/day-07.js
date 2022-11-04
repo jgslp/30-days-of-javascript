2bb20b6e22d763ad0d91c93a4940a03604e30bcb
Jacqueline Gellick <jgellick@gmail.com>
2022-11-04 21:27:20 +0100

    Completed exercises 1-1 and 1-2

---
+++ day-07/day-07.js
@@ -0,0 +1,23 @@
+// 30 DAYS OF JAVASCRIPT - DAY 07
+
+// LEVEL 1 EXERCISES
+
+// 1. Declare a function fullName and have it print out your full name
+
+// function printFullName() {
+//     let firstName = "Jacqueline";
+//     let lastName = "Gellick";
+//     let fullName = `${firstName} ${lastName}`;
+//     console.log(fullName);
+// }
+
+// printFullName()
+
+// 2. Declare a function fullName and now it takes firstName, lastName as a parameter and returns your full name
+
+// function printFullName(firstName, lastName) {
+//     let fullName = firstName + " " + lastName;
+//     console.log(fullName);
+// }
+
+// printFullName("Jacqueline", "Gellick")
