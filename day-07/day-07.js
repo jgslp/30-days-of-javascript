b649186ac38d3cfcf4566a63d3a854a0d7f7259b
Jacqueline Gellick <jgellick@gmail.com>
2022-11-06 18:34:32 +0100

    Changed console.logs in functions to return statements

--- day-07/day-07.js
+++ day-07/day-07.js
@@ -8,131 +8,138 @@
 //     let firstName = "Jacqueline";
 //     let lastName = "Gellick";
 //     let fullName = `${firstName} ${lastName}`;
-//     console.log(fullName);
+//     return fullName;
 // }
 
-// printFullName()
+// console.log(printFullName());
 
 // 2. Declare a function fullName and now it takes firstName, lastName as a parameter and returns your full name
 
 // function printFullName(firstName, lastName) {
 //     let fullName = firstName + " " + lastName;
-//     console.log(fullName);
+//     return fullName;
 // }
 
-// printFullName("Jacqueline", "Gellick")
+// console.log(printFullName("Jacqueline", "Gellick"));
 
 // 3. Declare a function addNumbers that takes two parameters and returns a sum
 
 // function addNumbers (num1, num2) {
 //     let sum = num1 + num2;
-//     console.log(sum);
+//     return sum;
 // }
 
-// addNumbers(10, 5);
+// console.log(addNumbers(10, 5));
 
 // 4. Write a function that calculates the area of a rectangle
 // function areaOfRectangle(length, width) {
 //     let area = length * width;
-//     console.log(area);
+//     return area;
 // }
 
-// areaOfRectangle(10, 5);
+// console.log(areaOfRectangle(10, 5));
 
 // 5. Write a function that calculates the perimeter of a rectangle
 // function perimeterOfRectangle (length, width) {
 //     let perimeter = 2 * (length + width);
-//     console.log(perimeter);
+//     return perimeter;
 // }
 
-// perimeterOfRectangle(10, 5);
+// console.log(perimeterOfRectangle(10, 5));
 
 // // 6. Write a function which calculates the volume of a rectangular prism
 // function volumeOfRectPrism(length, width, height) {
 //     let volume = length * width * height;
-//     console.log(volume);
+//     return volume;
 // }
 
-// volumeOfRectPrism(10, 5, 2);
+// console.log(volumeOfRectPrism(10, 5, 2));
 
 // 7. Write a function that calculates the area of a circle
 // function areaOfCircle(r) {
 //     let pi = 3.14;
 //     let area = pi * r * r;
-//     console.log(area);
+//     return area;
 // }
 
-// areaOfCircle(5);
+// console.log(areaOfCircle(5));
 
 // 8. Write a function that calculates the circumference of a circle
 
 // function circumferenceOfCircle(r) {
 //     let pi = 3.14;
 //     let circumference = 2 * pi * r;
-//     console.log(circumference);
+//     return (circumference);
 // }
 
-// circumferenceOfCircle(5);
+// console.log(circumferenceOfCircle(5));
 
 // 9. Write a function that calculates density
 // function density(mass, volume) {
 //     let density = mass / volume;
-//     console.log(density);
+//     return density;
 // }
 
-// density(10, 5);
+// console.log(density(10, 5));
 
 // 10. Write a function that calculates the speed of a moving object
 // function speed(distance, time) {
 //     let speed = distance / time;
-//     console.log(speed);
+//     return speed;
 // }
 
-// speed(10, 5);
+// console.log(speed(10, 5));
 
-// 11. write a function that calculates weight
+// // 11. write a function that calculates weight
 // function weight(mass, gravity) {
 //     let weight = mass * gravity;
-//     console.log(weight);
+//     return weight;
 // }
 
-// weight(50, 8);
+// console.log(weight(50, 8));
 
 // 12. Write a function that converts C to F
 // function convertCToF(degrees) {
 //     let fahrenheit = (degrees * 9 / 5) + 32;
-//     console.log(fahrenheit);
+//     return (fahrenheit);
 // }
 
-// convertCToF(22);
+// console.log(convertCToF(22));
 
-// 13. Write a function that calculates bmi. Check if a person is underweight, normal, overweight, or obese
+// // 13. Write a function that calculates bmi. Check if a person is underweight, normal, overweight, or obese
 // function calculateBMI(weightInKG, heightInM) {
 //     let bmi = weightInKG / (heightInM * heightInM);
 //     if (bmi >= 30) {
-//         console.log("obese")
+//         return ("obese")
 //     } else if (bmi >= 25) {
-//         console.log("overweight")
+//         return ("overweight")
 //     } else if (bmi >= 18.5) {
-//         console.log("normal weight") 
+//         return ("normal weight") 
 //     } else {
-//         console.log("underweight")
+//         return ("underweight")
 //     }
 // }
 
-// calculateBMI(50, 1.57);
+// console.log(calculateBMI(50, 1.57));
 
 // 14. Write a function called checkSeason, checks month parameter
 // function checkSeason(month) {
 //     if (month === "december" || month === "january" || month === "february") {
-//         console.log("winter");
+//         return ("winter");
 //     } else if (month === "march" || month === "april" || month === "may") {
-//         console.log("spring");
+//         return ("spring");
 //     } else if (month === "june" || month === "july" || month === "august") {
-//         console.log("summer");
+//         return ("summer");
 //     } else {
-//         console.log("autumn");
+//         return ("autumn");
 //     }
 // }
 
-// checkSeason("november");
+// console.log(checkSeason("november"));
+
+// 15. Write a function findMax that takes three arguments and returns their maximum without using Math.max method
+// function findMax(num1, num2, num3) {
+//    let array = [num1, num2, num3] {
+
+//    }
+// }
